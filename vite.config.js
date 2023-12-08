8dc62408e1a5f8d24aaf63684df155d0039bcf3b
mzlzyCA <87289074+mzlzyCA@users.noreply.github.com>
2023-12-08 18:08:46 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import { VantResolver } from '@vant/auto-import-resolver';
 
 
 export default defineConfig({
-  base:"./",
+  base:"./BAOBEIZHANBU",
   plugins: [
     vue(),
   //   vitePluginVueSvgIcons({ 
