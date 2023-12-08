e60fd395f4e8be9e41bec9ea411f33ecee566290
NineOceans <44770303+LyuLumos@users.noreply.github.com>
2023-12-08 07:41:03 +0000

    change config

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -4,15 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
+  base:'./',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
